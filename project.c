2abe1fcf16a60c44cef3a70841b172591460a624
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-12-20 18:41:19 +0100

    Correcting the action of the sieveOfErastotenes

--- project.c
+++ project.c
@@ -2,97 +2,87 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void sieveOfEratosthenes1(int m, int n, int* array) 
-{
-    bool* isPrime = (bool*)malloc((n - m + 1) * sizeof(bool));
+void sieveOfEratosthenes1(int m, int n, int** array, int* size) {
+    int isPrimeSize = n - m + 1;
+    bool* isPrime = (bool*)malloc(isPrimeSize * sizeof(bool));
 
-    printf("Rozmiar: %d\n", sizeof(isPrime));
-
-    for (int i = 0; i < sizeof(isPrime)/sizeof(bool); i++) 
-    {
+    for (int i = 0; i < isPrimeSize; i++) {
         isPrime[i] = true;
-        printf("%d, ", isPrime[i]);
     }
 
-    printf("\n");
-
-    for (int p = 2; p * p <= n; p++) 
-    {
-        if (isPrime[p] == true) 
-        {
-            for (int i = p * p; i <= n; i += p) 
-            {
-                isPrime[i] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i - m; j < isPrimeSize; j += i) {
+                isPrime[j] = false;
             }
         }
     }
 
-    int j = 0; 
-    for (int i = 2; i <= n; i++) 
-    {
-        if (isPrime[i]) 
-        {
-            array[j++] = i;
+    int j = 0;
+    for (int i = 0; i < isPrimeSize; i++) {
+        if (isPrime[i]) {
+            (*array)[j++] = i + m;
         }
     }
 
-    array = realloc(array, j * sizeof(int));
+    *array = realloc(*array, j * sizeof(int));
+    *size = j;
 
     free(isPrime);
 }
 
-int main() 
-{
+int main() {
     int m, n;
 
-    while (1)
-    {
-        printf("%s\n", "Wprowadz dolna granica:");
+    // Input
+    while (1) {
+        printf("%s\n", "Enter the lower bound:");
         scanf_s("%d", &m);
 
-        if (m < 2) 
-		{
-			printf("%s\n", "Dolna granica musi byc wieksza od 1!");
-			continue;
-		}
-
-        printf("%s\n", "Wprowadz gorna granice:");
-        scanf_s("%d", &n);
-
-        if (m < n) 
-        {
-            break;  
+        if (m < 2) {
+            printf("%s\n", "Lower bound must be greater than 1!");
+            continue;
         }
 
-        printf("%s\n", "Dolna granica musi byc mniejsza od gornej granicy!");
+        printf("%s\n", "Enter the upper bound:");
+        scanf_s("%d", &n);
+
+        if (m < n) {
+            break;
+        }
+
+        printf("%s\n", "Lower bound must be less than the upper bound!");
     }
 
+    // Generate an array of all numbers from m to n
     int arraySize = n - m + 1;
-
     int* numbersArray = (int*)malloc(arraySize * sizeof(int));
 
-    if (numbersArray == NULL) 
-    {
+    if (numbersArray == NULL) {
         printf("Memory allocation failed.\n");
-        return 1;  
+        return 1;
     }
 
-    for (int i = 0; i < arraySize; i++) 
-    {
+    for (int i = 0; i < arraySize; i++) {
         numbersArray[i] = m + i;
     }
 
-    // Sekwencyjnie
-    sieveOfEratosthenes1(m, n, numbersArray);
+    // Apply the Sieve of Eratosthenes to filter prime numbers
+    int newSize = 0;
+    sieveOfEratosthenes1(m, n, &numbersArray, &newSize);
 
+    // Display the modified array with prime numbers
     printf("Modified array with prime numbers: [");
-    for (int i = 0; i < n - 2; i++) 
-    {
+    for (int i = 0; i < newSize - 1; i++) {
         printf("%d, ", numbersArray[i]);
     }
-    printf("%d]\n", numbersArray[n - 2]);
+    if (newSize > 0) {
+        printf("%d", numbersArray[newSize - 1]);
+    }
+    printf("]\n");
 
+    // Free allocated memory
     free(numbersArray);
 
-    return 0; 
+    return 0;
 }
