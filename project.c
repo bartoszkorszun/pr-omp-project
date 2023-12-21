ded23ff69337d18c27928639f08d0415cbdaa0a2
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-12-21 16:27:27 +0100

    Adding fully working eratostenes sieve

--- project.c
+++ project.c
@@ -2,44 +2,57 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void sieveOfEratosthenes1(int m, int n, int** array, int* size) {
-    int isPrimeSize = n - m + 1;
-    bool* isPrime = (bool*)malloc(isPrimeSize * sizeof(bool));
+void sieveOfEratosthenes1(int m, int n, int** array, int* size) 
+{
+    for (int i = 2; i * i <= n; i++) 
+	{
+		for (int j = 0; j < n - m + 1;)
+		{
+			if ((*array)[j] % i == 0 && (*array)[j] != i)
+			{
+				(*array)[j] = 1;
+                j += i;
+			}
+			else
+			{
+				j++;
+			}
+		}
+	}
 
-    for (int i = 0; i < isPrimeSize; i++) {
-        isPrime[i] = true;
-    }
-
-    for (int i = 2; i * i <= n; i++) {
-        if (isPrime[i]) {
-            for (int j = i * i - m; j < isPrimeSize; j += i) {
-                isPrime[j] = false;
-            }
-        }
+    for (int i = 0; i < n - m + 1; i++)
+    {
+        if ((*array)[i] == 1)
+		{
+			(*array)[i] = 0;
+		}
     }
 
     int j = 0;
-    for (int i = 0; i < isPrimeSize; i++) {
-        if (isPrime[i]) {
-            (*array)[j++] = i + m;
+    for (int i = 0; i < n - m + 1; i++) 
+    {
+        if ((*array)[i])
+        {
+            (*array)[j++] = m + i;
         }
     }
 
     *array = realloc(*array, j * sizeof(int));
     *size = j;
-
-    free(isPrime);
 }
 
-int main() {
+int main() 
+{
     int m, n;
 
     // Input
-    while (1) {
+    while (1) 
+    {
         printf("%s\n", "Enter the lower bound:");
         scanf_s("%d", &m);
 
-        if (m < 2) {
+        if (m < 2) 
+        {
             printf("%s\n", "Lower bound must be greater than 1!");
             continue;
         }
@@ -47,7 +60,8 @@ int main() {
         printf("%s\n", "Enter the upper bound:");
         scanf_s("%d", &n);
 
-        if (m < n) {
+        if (m < n) 
+        {
             break;
         }
 
@@ -58,12 +72,14 @@ int main() {
     int arraySize = n - m + 1;
     int* numbersArray = (int*)malloc(arraySize * sizeof(int));
 
-    if (numbersArray == NULL) {
+    if (numbersArray == NULL) 
+    {
         printf("Memory allocation failed.\n");
         return 1;
     }
 
-    for (int i = 0; i < arraySize; i++) {
+    for (int i = 0; i < arraySize; i++) 
+    {
         numbersArray[i] = m + i;
     }
 
@@ -73,10 +89,12 @@ int main() {
 
     // Display the modified array with prime numbers
     printf("Modified array with prime numbers: [");
-    for (int i = 0; i < newSize - 1; i++) {
+    for (int i = 0; i < newSize - 1; i++) 
+    {
         printf("%d, ", numbersArray[i]);
     }
-    if (newSize > 0) {
+    if (newSize > 0) 
+    {
         printf("%d", numbersArray[newSize - 1]);
     }
     printf("]\n");
