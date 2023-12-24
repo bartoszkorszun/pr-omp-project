d95effd6bc8bbf057ed72dff819cd5d1af2b4081
Bartosz Korszun <bartoszkorszun@gmail.com>
2023-12-24 01:12:04 +0100

    First approach to parallel processing

--- project.c
+++ project.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+#include <omp.h>
 
 void sieveOfEratosthenes1(int m, int n, int** array, int* size) 
 {
@@ -41,9 +43,50 @@ void sieveOfEratosthenes1(int m, int n, int** array, int* size)
     *size = j;
 }
 
+void sieveOfEratosthenes2(int index, int m, int n, int** array, int* size)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        for (int j = index; j < n - m + 1;)
+        {
+            if ((*array)[j] % i == 0 && (*array)[j] != i)
+            {
+                (*array)[j] = 1;
+                j += i;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+
+    for (int i = index; i < n - m + 1; i++)
+    {
+        if ((*array)[i] == 1)
+        {
+            (*array)[i] = 0;
+        }
+    }
+
+    int j = 0;
+#pragma omp critical
+    for (int i = index; i < n - m + 1; i++)
+    {
+        if ((*array)[i])
+        {
+            (*array)[j++] = m + i;
+        }
+    }
+
+#pragma omp atomic
+    *size += j;
+}
+
 int main() 
 {
-    int m, n;
+    int m, n, choice;
+    clock_t start, end;
 
     // Input
     while (1) 
@@ -60,12 +103,21 @@ int main()
         printf("%s\n", "Enter the upper bound:");
         scanf_s("%d", &n);
 
-        if (m < n) 
+        if (m >= n)         
         {
-            break;
+            printf("%s\n", "Lower bound must be less than the upper bound!");
+            continue;
         }
 
-        printf("%s\n", "Lower bound must be less than the upper bound!");
+        printf("%s\n %s\n %s\n", "Choose:", "1 - Sequential", "2 - Parallel");
+        scanf_s("%d", &choice);
+
+        if (choice == 1 || choice == 2) 
+		{
+            break;
+		}
+
+		printf("%s\n", "Invalid choice!");
     }
 
     // Generate an array of all numbers from m to n
@@ -83,9 +135,35 @@ int main()
         numbersArray[i] = m + i;
     }
 
-    // Apply the Sieve of Eratosthenes to filter prime numbers
     int newSize = 0;
-    sieveOfEratosthenes1(m, n, &numbersArray, &newSize);
+    
+    if (choice == 1) 
+	{
+		printf("%s\n", "Sequential");
+        // Apply the Sieve of Eratosthenes to filter prime numbers
+        start = clock();
+        sieveOfEratosthenes1(m, n, &numbersArray, &newSize);
+        end = clock();
+	}
+	else if (choice == 2) 
+	{
+		printf("%s\n", "Parallel");
+        start = clock();
+        // Apply the Sieve of Eratosthenes to filter prime numbers
+#pragma omp parallel for
+        for (int i = 0; i < arraySize/16; i += 16) 
+		{
+            sieveOfEratosthenes2(i, numbersArray[i], numbersArray[i + 16], &numbersArray, &newSize);
+		}
+
+        numbersArray = realloc(numbersArray, newSize * sizeof(int));
+        end = clock();
+	}
+	else 
+	{
+		printf("%s\n", "Oops something went wrong!");
+		return 1;
+	}
 
     // Display the modified array with prime numbers
     printf("Modified array with prime numbers: [");
@@ -98,6 +176,9 @@ int main()
         printf("%d", numbersArray[newSize - 1]);
     }
     printf("]\n");
+
+    // Display the execution time
+    printf("Execution time: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     // Free allocated memory
     free(numbersArray);
